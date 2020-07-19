18afbdc0c400dfd3ee93b1b89187b8baa8238a36
Christian Herlambang <ChristianRudy.Herlambang@sampoerna.com>
2020-07-19 15:07:13 +0700

    Add bug

--- helloworld/apps.py
+++ helloworld/apps.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 import newrelic.agent
 newrelic.agent.initialize('/home/ec2-user/newrelic.ini')
 
+
 from django.apps import AppConfig
 
-
 class HelloworldConfig(AppConfig):
+    10/0
     name = 'helloworld'
