7e49179993753e52ab332c5b02db2125cc76fadc
Christian Herlambang <ChristianRudy.Herlambang@sampoerna.com>
2020-07-19 17:07:37 +0700

    Revert "Add bug"
    This reverts commit 18afbdc0c400dfd3ee93b1b89187b8baa8238a36.

--- helloworld/apps.py
+++ helloworld/apps.py
@@ -3,9 +3,8 @@ from __future__ import unicode_literals
 import newrelic.agent
 newrelic.agent.initialize('/home/ec2-user/newrelic.ini')
 
-
 from django.apps import AppConfig
 
+
 class HelloworldConfig(AppConfig):
-    10/0
     name = 'helloworld'
